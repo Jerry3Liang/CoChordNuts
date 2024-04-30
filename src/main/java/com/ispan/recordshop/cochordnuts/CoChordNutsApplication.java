f5522e949df58df5d62e1e2a24a8f8860e748d06
JerryLiang <a810317g@tmu.edu.tw>
2024-04-30 14:51:09 +0800

    建立 Spring Boot 專案

---
+++ src/main/java/com/ispan/recordshop/cochordnuts/CoChordNutsApplication.java
@@ -0,0 +1,13 @@
+package com.ispan.recordshop.cochordnuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoChordNutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoChordNutsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ispan/recordshop/cochordnuts/CoChordNutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispan.recordshop.cochordnuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoChordNutsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
