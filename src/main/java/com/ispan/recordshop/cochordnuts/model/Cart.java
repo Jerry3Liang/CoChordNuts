a0671703000927a3519a3f3c59f4a93dab2e0368
戎少英 <>
2024-05-08 11:41:51 +0800

    Cart類別新增Getter Setter

--- src/main/java/com/ispan/recordshop/cochordnuts/model/Cart.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/model/Cart.java
@@ -22,11 +22,44 @@ public class Cart {
 	
 	@Column(name = "memberNo")
 	private Integer memberNo;
-
+	
 	@Column(name = "productNo")
 	private Integer productNo;
 	
 	@Column(name = "count")
 	private Integer count;
+
+	public Integer getCartNo() {
+		return cartNo;
+	}
+
+	public void setCartNo(Integer cartNo) {
+		this.cartNo = cartNo;
+	}
+
+	public Integer getMemberNo() {
+		return memberNo;
+	}
+
+	public void setMemberNo(Integer memberNo) {
+		this.memberNo = memberNo;
+	}
+
+	public Integer getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(Integer productNo) {
+		this.productNo = productNo;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
 	
 }
