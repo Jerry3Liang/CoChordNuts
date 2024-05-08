2e18c691238a1af391756254b700a551be6ad7ea
DESKTOP-RAE7E6C\User <mini3924@gmail.com>
2024-05-08 13:49:33 +0800

    Order與Orderdetail關聯

--- src/main/java/com/ispan/recordshop/cochordnuts/model/OrderDetail.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/model/OrderDetail.java
@@ -4,17 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="order_detail")
+@Table(name="orderDetail")
 public class OrderDetail {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer orderDetailNo;//訂單明細流水號
 	
-	private Integer orderNo;//訂單編號
+	@ManyToOne
+	@JoinColumn(name="orderNo" ,referencedColumnName = "orderNo")
+	private Orders orderNo;//訂單編號
 	
 	private String productName;//名稱
 	
@@ -26,77 +30,79 @@ public class OrderDetail {
 	
 	private float discount;//打折數
 	
+//	@ManyToOne //與Product連接
+//	@JoinColumn(name="productNo" ,referencedColumnName = "productNo")
+//	private Product productNo;//商品編號
 	private Integer productNo;//商品編號
 	
 	public OrderDetail() {
 
 	}
-	
+
 	public Integer getOrderDetailNo() {
 		return orderDetailNo;
-	}
-
-	public Integer getOrderNo() {
-		return orderNo;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public Integer getCount() {
-		return count;
-	}
-
-	public Integer getUnitPrice() {
-		return unitPrice;
-	}
-
-	public Integer getPay() {
-		return pay;
-	}
-
-	public float getDiscount() {
-		return discount;
-	}
-
-	public Integer getProductNo() {
-		return productNo;
 	}
 
 	public void setOrderDetailNo(Integer orderDetailNo) {
 		this.orderDetailNo = orderDetailNo;
 	}
 
-	public void setOrderNo(Integer orderNo) {
+	public Orders getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(Orders orderNo) {
 		this.orderNo = orderNo;
+	}
+
+	public String getProductName() {
+		return productName;
 	}
 
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
 
+	public Integer getCount() {
+		return count;
+	}
+
 	public void setCount(Integer count) {
 		this.count = count;
+	}
+
+	public Integer getUnitPrice() {
+		return unitPrice;
 	}
 
 	public void setUnitPrice(Integer unitPrice) {
 		this.unitPrice = unitPrice;
 	}
 
+	public Integer getPay() {
+		return pay;
+	}
+
 	public void setPay(Integer pay) {
 		this.pay = pay;
+	}
+
+	public float getDiscount() {
+		return discount;
 	}
 
 	public void setDiscount(float discount) {
 		this.discount = discount;
 	}
 
+	public Integer getProductNo() {
+		return productNo;
+	}
+
 	public void setProductNo(Integer productNo) {
 		this.productNo = productNo;
 	}
-	
 
-	
+
 
 }
--- src/main/java/com/ispan/recordshop/cochordnuts/model/Orders.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/model/Orders.java
@@ -1,11 +1,16 @@
 package com.ispan.recordshop.cochordnuts.model;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,7 +20,19 @@ public class Orders {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer orderNo;//訂單編號
-		
+
+	
+	@OneToMany(mappedBy = "orderNo" ,cascade = CascadeType.ALL)
+	private List<OrderDetail> orderDetail;//訂單編號
+	
+	//與CustomerCase連接
+//	@OneToMany(mappedBy = "orderNo" ,cascade = CascadeType.ALL)
+//	private List<CustomerCase> CustomerCase;//訂單編號
+	
+
+//	@ManyToOne //與Member連接
+//	@JoinColumn(name="memberNo" ,referencedColumnName = "memberNo")
+//	private Member memberNo;//訂單編號
 	private Integer memberNo;//會員編號
 	
 	private Date createDate;//下單日
@@ -61,162 +78,170 @@ public class Orders {
 		return orderNo;
 	}
 
-	public Integer getMemberNo() {
-		return memberNo;
-	}
-
-	public Date getCreateDate() {
-		return createDate;
-	}
-
-	public Date getPreparationDate() {
-		return preparationDate;
-	}
-
-	public Date getDispatchDate() {
-		return dispatchDate;
-	}
-
-	public Date getCompleteDate() {
-		return completeDate;
-	}
-
-	public Date getCnacelDate() {
-		return cnacelDate;
-	}
-
-	public Date getReturnDate() {
-		return returnDate;
-	}
-
-	public String getDeliverType() {
-		return deliverType;
-	}
-
-	public Integer getFreight() {
-		return freight;
-	}
-
-	public Integer getTotalCount() {
-		return totalCount;
-	}
-
-	public Integer getTotalPay() {
-		return totalPay;
-	}
-
-	public String getPayment() {
-		return payment;
-	}
-
-	public String getCreditCardNo() {
-		return creditCardNo;
-	}
-
-	public String getReceiptType() {
-		return receiptType;
-	}
-
-	public String getReceiptNo() {
-		return receiptNo;
-	}
-
-	public String getRecipient() {
-		return recipient;
-	}
-
-	public String getRecipientAddress() {
-		return recipientAddress;
-	}
-
-	public String getRecipientPhone() {
-		return recipientPhone;
-	}
-
-	public String getNote() {
-		return note;
-	}
-
 	public void setOrderNo(Integer orderNo) {
 		this.orderNo = orderNo;
+	}
+
+	public List<OrderDetail> getOrderDetail() {
+		return orderDetail;
+	}
+
+	public void setOrderDetail(List<OrderDetail> orderDetail) {
+		this.orderDetail = orderDetail;
+	}
+
+	public Integer getMemberNo() {
+		return memberNo;
 	}
 
 	public void setMemberNo(Integer memberNo) {
 		this.memberNo = memberNo;
 	}
 
+	public Date getCreateDate() {
+		return createDate;
+	}
+
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
+	}
+
+	public Date getPreparationDate() {
+		return preparationDate;
 	}
 
 	public void setPreparationDate(Date preparationDate) {
 		this.preparationDate = preparationDate;
 	}
 
+	public Date getDispatchDate() {
+		return dispatchDate;
+	}
+
 	public void setDispatchDate(Date dispatchDate) {
 		this.dispatchDate = dispatchDate;
+	}
+
+	public Date getCompleteDate() {
+		return completeDate;
 	}
 
 	public void setCompleteDate(Date completeDate) {
 		this.completeDate = completeDate;
 	}
 
+	public Date getCnacelDate() {
+		return cnacelDate;
+	}
+
 	public void setCnacelDate(Date cnacelDate) {
 		this.cnacelDate = cnacelDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
 	}
 
 	public void setReturnDate(Date returnDate) {
 		this.returnDate = returnDate;
 	}
 
+	public String getDeliverType() {
+		return deliverType;
+	}
+
 	public void setDeliverType(String deliverType) {
 		this.deliverType = deliverType;
+	}
+
+	public Integer getFreight() {
+		return freight;
 	}
 
 	public void setFreight(Integer freight) {
 		this.freight = freight;
 	}
 
+	public Integer getTotalCount() {
+		return totalCount;
+	}
+
 	public void setTotalCount(Integer totalCount) {
 		this.totalCount = totalCount;
+	}
+
+	public Integer getTotalPay() {
+		return totalPay;
 	}
 
 	public void setTotalPay(Integer totalPay) {
 		this.totalPay = totalPay;
 	}
 
+	public String getPayment() {
+		return payment;
+	}
+
 	public void setPayment(String payment) {
 		this.payment = payment;
+	}
+
+	public String getCreditCardNo() {
+		return creditCardNo;
 	}
 
 	public void setCreditCardNo(String creditCardNo) {
 		this.creditCardNo = creditCardNo;
 	}
 
+	public String getReceiptType() {
+		return receiptType;
+	}
+
 	public void setReceiptType(String receiptType) {
 		this.receiptType = receiptType;
+	}
+
+	public String getReceiptNo() {
+		return receiptNo;
 	}
 
 	public void setReceiptNo(String receiptNo) {
 		this.receiptNo = receiptNo;
 	}
 
+	public String getRecipient() {
+		return recipient;
+	}
+
 	public void setRecipient(String recipient) {
 		this.recipient = recipient;
+	}
+
+	public String getRecipientAddress() {
+		return recipientAddress;
 	}
 
 	public void setRecipientAddress(String recipientAddress) {
 		this.recipientAddress = recipientAddress;
 	}
 
+	public String getRecipientPhone() {
+		return recipientPhone;
+	}
+
 	public void setRecipientPhone(String recipientPhone) {
 		this.recipientPhone = recipientPhone;
+	}
+
+	public String getNote() {
+		return note;
 	}
 
 	public void setNote(String note) {
 		this.note = note;
 	}
-	
+
 	
 	
 }
