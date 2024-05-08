43675e571469b3a42061f433e1df7c2206aa201a
lin888688 <164441308+lin888688@users.noreply.github.com>
2024-05-08 16:08:44 +0800

    Update ProductStyle.java

--- src/main/java/com/ispan/recordshop/cochordnuts/model/ProductStyle.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/model/ProductStyle.java
@@ -1,31 +1,52 @@
+
 package com.ispan.recordshop.cochordnuts.model;
 
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "product_style")
 public class ProductStyle {
-	
-	@OneToMany(
-			mappedBy = "productStyle",
-			cascade = CascadeType.ALL)
+
+	@OneToMany(mappedBy = "productStyle", cascade = CascadeType.ALL)
 	private Set<Product> products;
-	
+
+	@ManyToMany(mappedBy = "favoriteMusicType")
+	private List<Member> members = new ArrayList<>(); // 喜好此音樂類型的會員
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer styleNo; //商品類型編號
-	
-	private String styleType; //商品類型描述
+	private Integer styleNo; // 商品類型編號
+
+	private String styleType; // 商品類型描述
 
 	public ProductStyle() {
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	public List<Member> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<Member> members) {
+		this.members = members;
 	}
 
 	public Integer getStyleNo() {
@@ -44,13 +65,6 @@ public class ProductStyle {
 		this.styleType = styleType;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(Set<Product> products) {
-		this.products = products;
-	}
 	
 
 }
