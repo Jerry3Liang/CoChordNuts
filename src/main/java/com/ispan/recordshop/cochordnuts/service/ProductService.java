2b033700dbde63579caafa43e1a23c932587fb56
DESKTOP-RAE7E6C\User <abbywupp@gmail.com>
2024-05-09 16:38:20 +0800

    多條件查詢

--- src/main/java/com/ispan/recordshop/cochordnuts/service/ProductService.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.ispan.recordshop.cochordnuts.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.Session;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,8 +16,22 @@ import org.springframework.stereotype.Service;
 import com.ispan.recordshop.cochordnuts.model.Product;
 import com.ispan.recordshop.cochordnuts.repository.ProductRepository;
 
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+
 @Service
 public class ProductService {
+	
+	@PersistenceContext
+	private Session session;
+	public Session getSession() {
+		return this.session;
+	}
 
 	@Autowired
 	private ProductRepository productRepo;
@@ -52,6 +70,97 @@ public class ProductService {
 		}
 		return null;
 	}
+	
+	//多條件查詢
+	public List<Product> search(JSONObject obj) {
+		//判斷欄位是否空白
+		Integer productNo = obj.isNull("productNo")? null : obj.getInt("productNo");
+		String productName = obj.isNull("productName")? null : obj.getString("productName");
+		Double startPrice = obj.isNull("startPrice") ? null : obj.getDouble("startPrice") ;
+		Double endPrice = obj.isNull("endPrice") ? null : obj.getDouble("endPrice") ;
+		String startDate = obj.isNull("startDate")? null : obj.getString("startDate");
+		String endDate = obj.isNull("endDate")? null : obj.getString("endDate");
+		String artistName = obj.isNull("artistName")? null : obj.getString("artistName");
+		Integer style = obj.isNull("style")? null : obj.getInt("style");
+		Integer language = obj.isNull("language")? null : obj.getInt("language");
+		
+		//設定排列預設值
+		int rows = obj.isNull("rows") ? 15 : obj.getInt("rows");
+		String order = obj.isNull("order") ? "productNo" : obj.getString("order");
+		boolean direction = obj.isNull("direction") ? false : obj.getBoolean("direction");
+		
+		//Criteria Connect
+		CriteriaBuilder criteriaBuilder = this.getSession().getCriteriaBuilder();
+		CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+		
+		//from product table
+		Root<Product> table = criteriaQuery.from(Product.class);
+		
+		//where start
+		List<Predicate> predicates = new ArrayList<>();
+		
+		if(productNo != null) {
+			Predicate p1 = criteriaBuilder.equal(table.get("productNo"), productNo);
+			predicates.add(p1);
+		}
+		if(productName != null && productName.length() != 0) {
+			Predicate p2 = criteriaBuilder.like(table.get("productName"), "%"+productName+"%");
+			predicates.add(p2);
+		}
+		if(startPrice != null) {
+			predicates.add(criteriaBuilder.greaterThanOrEqualTo(table.get("unitPrice"), startPrice));
+		}
+		if(endPrice != null) {
+			predicates.add(criteriaBuilder.lessThanOrEqualTo(table.get("unitPrice"), endPrice));
+		}
+		//日期比較 處理~~~~
+//		if(startDate != null) {
+//			java.util.Date temp = DatetimeConverter.parse(startDate, "yyyy-MM-dd");
+//			predicates.add(criteriaBuilder.greaterThan(table.get("publishedDate"), temp));
+//		}
+//		if(endDate != null) {
+//			java.util.Date temp = DatetimeConverter.parse(endDate, "yyyy-MM-dd");
+//			predicates.add(criteriaBuilder.lessThan(table.get("publishedDate"), temp));
+//		}
+		if(artistName != null && artistName.length() != 0) {
+			Predicate p3 = criteriaBuilder.like(table.get("artist"), "%"+artistName+"%");
+			predicates.add(p3);
+		}
+		//下拉選項用數字對照
+		if(style != null && style != 0) {
+			predicates.add(criteriaBuilder.equal(table.get("productStyle"), style));
+		}
+		//下拉選項用數字對照
+		if(language != null && language != 0) {
+			predicates.add(criteriaBuilder.equal(table.get("language"), language));
+		}
+		//where end		
+		
+		//order by
+		if(direction) {
+			criteriaQuery = criteriaQuery.orderBy(criteriaBuilder.desc(table.get(order)));
+		} else {
+			criteriaQuery = criteriaQuery.orderBy(criteriaBuilder.asc(table.get(order)));
+		}
+		
+		TypedQuery<Product> typedQuery = this.getSession().createQuery(criteriaQuery)
+				.setMaxResults(rows);
+		
+		List<Product> result = typedQuery.getResultList();
+		if(result != null && !result.isEmpty()) {
+			return result;
+		} else {
+			return null;
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
