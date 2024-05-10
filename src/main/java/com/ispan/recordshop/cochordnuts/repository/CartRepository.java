a3b1e73885966c3fff86748a0392a03188a8b593
戎少英 <>
2024-05-10 17:34:42 +0800

    更新CartRepository->新增query

--- src/main/java/com/ispan/recordshop/cochordnuts/repository/CartRepository.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/repository/CartRepository.java
@@ -1,10 +1,20 @@
 package com.ispan.recordshop.cochordnuts.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ispan.recordshop.cochordnuts.model.Cart;
 import com.ispan.recordshop.cochordnuts.model.CartId;
 
 public interface CartRepository extends JpaRepository<Cart, CartId> {
 
+	@Query("select c from Cart c where c.cartId.memberId = :memberId ")
+	public List<Cart> findMemberCartProducts(@Param("memberId") Integer memberId);
+	
+	@Query("select c from Cart c where c.cartId.memberId = :x and c.cartId.productId = :y")
+	public Cart findByMemberAndProducts(@Param("x") Integer cartMemberId, @Param("y") Integer cartProductId);
+	
 }
