4edb914dfa364e42adf78a43cefbf12d69ff621e
DESKTOP-RAE7E6C\User <abbywupp@gmail.com>
2024-05-13 15:22:07 +0800

    artist update

--- src/main/java/com/ispan/recordshop/cochordnuts/controller/ArtistController.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/controller/ArtistController.java
@@ -1,16 +1,8 @@
 package com.ispan.recordshop.cochordnuts.controller;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import java.util.List;
-
->>>>>>> parent of 15584fd (Product 多條件查詢 & Artist依名稱查詢)
-=======
 import java.util.List;
 
 import org.json.JSONObject;
->>>>>>> Product類型功能
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,37 +19,24 @@ public class ArtistController {
 
 	@Autowired
 	private ArtistService artistService;
-	
+
 	@PostMapping("/product/artist/add")
 	public Artist insert(@RequestBody Artist artist) {
 		return artistService.insert(artist);
 	}
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 	@GetMapping("/product/artist/find")
-	public List<Artist> findAll(){
-		return artistService.findAll();
-	}
-	
->>>>>>> parent of 15584fd (Product 多條件查詢 & Artist依名稱查詢)
-=======
-	@GetMapping("/product/artist/find")
-	public String findAll(){
+	public String findAll() {
 //		return artistService.findAll();
 		List<Artist> result = artistService.findAll();
 		return result.toString();
 	}
-	
+
 	@PostMapping("/product/artist/findByName")
 	public String insert(@RequestBody String obj) {
 		JSONObject json = new JSONObject(obj);
-		String artistName = json.isNull("artistName")? null : json.getString("artistName");
+		String artistName = json.isNull("artistName") ? null : json.getString("artistName");
 		return artistService.findByName(artistName).toString();
 	}
-	
-	
->>>>>>> Product類型功能
-	
+
 }
--- src/main/java/com/ispan/recordshop/cochordnuts/service/ArtistService.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/service/ArtistService.java
@@ -1,16 +1,9 @@
 package com.ispan.recordshop.cochordnuts.service;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import java.util.List;
-
->>>>>>> parent of 15584fd (Product 多條件查詢 & Artist依名稱查詢)
-=======
 import java.util.List;
 
 import org.hibernate.Session;
->>>>>>> Product類型功能
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,42 +14,27 @@ import jakarta.persistence.PersistenceContext;
 
 @Service
 public class ArtistService {
-	
+
 	@PersistenceContext
 	private Session session;
+
 	public Session getSession() {
 		return this.session;
 	}
-	
+
 	@Autowired
 	private ArtistRepository artistRepository;
-	
+
 	public Artist insert(Artist artist) {
 		return artistRepository.save(artist);
 	}
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> Product類型功能
-	public List<Artist> findAll(){
+
+	public List<Artist> findAll() {
 		return artistRepository.findAll();
 	}
-	
-<<<<<<< HEAD
-	
-	
->>>>>>> parent of 15584fd (Product 多條件查詢 & Artist依名稱查詢)
-=======
-	public List<Artist> findByName(String name){
-		
+
+	public List<Artist> findByName(String name) {
 		return artistRepository.findByNameLike(name);
 	}
-	
-	
-	
-	
->>>>>>> Product類型功能
 
 }
