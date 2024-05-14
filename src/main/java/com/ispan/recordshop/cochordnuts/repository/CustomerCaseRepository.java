eb8d7f6b7be53d5342403950ba82b8ee945e5e98
lin888688 <164441308+lin888688@users.noreply.github.com>
2024-05-14 17:53:26 +0800

    更新CustomerCaseRepository

--- src/main/java/com/ispan/recordshop/cochordnuts/repository/CustomerCaseRepository.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/repository/CustomerCaseRepository.java
@@ -1,8 +1,16 @@
 package com.ispan.recordshop.cochordnuts.repository;
 
 import com.ispan.recordshop.cochordnuts.model.CustomerCase;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface CustomerCaseRepository extends JpaRepository<CustomerCase, Integer> {
+
+    @Query("from CustomerCase c where c.customerCaseMember.memberNo = :memberNo")
+    public List<CustomerCase> findByMemberNo(Integer memberNo);
 }
