6c25ce902ecee8ad97a2a324605ab2638e88e567
Abby <abbywupp@gmail.com>
2024-05-14 20:11:44 +0800

    ProductDTO 新增getter setter

--- src/main/java/com/ispan/recordshop/cochordnuts/dto/ProductDTO.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/dto/ProductDTO.java
@@ -18,8 +18,123 @@ public class ProductDTO {
 	private String artistType; //藝人
 	private String languageType; //語言
 	private String musicYear; //音樂年份
-
+	
+	
 	public ProductDTO() {
 	}
+
+
+	public Integer getProductNo() {
+		return productNo;
+	}
+
+
+	public void setProductNo(Integer productNo) {
+		this.productNo = productNo;
+	}
+
+
+	public String getProductName() {
+		return productName;
+	}
+
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+
+	public Integer getUnitPrice() {
+		return unitPrice;
+	}
+
+
+	public void setUnitPrice(Integer unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+
+	public String getDescribe() {
+		return describe;
+	}
+
+
+	public void setDescribe(String describe) {
+		this.describe = describe;
+	}
+
+
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+
+
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+
+	public double getDiscount() {
+		return discount;
+	}
+
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+
+	public String getStyleType() {
+		return styleType;
+	}
+
+
+	public void setStyleType(String styleType) {
+		this.styleType = styleType;
+	}
+
+
+	public String getArtistType() {
+		return artistType;
+	}
+
+
+	public void setArtistType(String artistType) {
+		this.artistType = artistType;
+	}
+
+
+	public String getLanguageType() {
+		return languageType;
+	}
+
+
+	public void setLanguageType(String languageType) {
+		this.languageType = languageType;
+	}
+
+
+	public String getMusicYear() {
+		return musicYear;
+	}
+
+
+	public void setMusicYear(String musicYear) {
+		this.musicYear = musicYear;
+	}
+	
+	
+	
+	
 
 }
