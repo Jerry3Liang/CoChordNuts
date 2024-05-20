6dd7c8daeba8a88d5ab205c75a30c639867f2d43
lin888688 <164441308+lin888688@users.noreply.github.com>
2024-05-20 16:56:57 +0800

    更新登入登出方法

--- src/main/java/com/ispan/recordshop/cochordnuts/controller/MemberInOutController.java
+++ src/main/java/com/ispan/recordshop/cochordnuts/controller/MemberInOutController.java
@@ -1,5 +1,7 @@
 package com.ispan.recordshop.cochordnuts.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.json.JSONObject;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ispan.recordshop.cochordnuts.model.Member;
 import com.ispan.recordshop.cochordnuts.service.impl.MemberService;
+import com.ispan.recordshop.cochordnuts.util.DatetimeConverter;
+import com.ispan.recordshop.cochordnuts.util.JsonWebTokenUtility;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -22,6 +27,9 @@ import jakarta.servlet.http.HttpSession;
 public class MemberInOutController {
 	@Autowired
 	private MemberService memberService;
+
+	@Autowired
+	private JsonWebTokenUtility jsonWebTokenUtility;
 
 	@PostMapping("/login")
 	public String handlerMethod(HttpSession session, @RequestBody String json) {
@@ -46,32 +54,55 @@ public class MemberInOutController {
 			responseJson.put("success", false);
 			responseJson.put("message", "登入失敗");
 		} else {
-			// Date currentLoginTime = new Date();
-			// Date lastLoginTime = member.getLastLoginTime();
-			// member.setLastLoginTime(currentLoginTime);
-			// memberService.logout(member);
-			session.setAttribute("user", member);
+			String lastLoginTime = DatetimeConverter.toString(new Date(), "yyyy-MM-dd HH:mm:ss");
+
+			session.setAttribute("loginMember", member);
+			session.setAttribute("lastLoginTime", lastLoginTime);
+
 			responseJson.put("success", true);
 			responseJson.put("message", "登入成功");
-			// responseJson.put("last_login_time", lastLoginTime);
+			responseJson.put("lastLoginTime", lastLoginTime);
 		}
+
+		JSONObject user = new JSONObject()
+				.put("memberNo", member.getMemberNo())
+				.put("email", member.getEmail());
+
+		String lastLoginTime = DatetimeConverter.toString(new Date(), "yyyy-MM-dd HH:mm:ss");
+		String token = jsonWebTokenUtility.createEncryptedToken(user.toString(), null);
+		responseJson.put("token", token);
+		responseJson.put("userName", member.getName());
+		responseJson.put("user", member);
+		responseJson.put("memberNo", member.getMemberNo());
+		responseJson.put("lastLoginTime", lastLoginTime);
+
 		return responseJson.toString();
 	}
 
 	// 登出
-	@GetMapping("/users/logout")
-	public String logoutAction(HttpServletRequest request, HttpSession session) {
+	@PostMapping("/logout")
+	public String logout(HttpSession session, @RequestBody String json) {
+		JSONObject responseJson = new JSONObject();
+		JSONObject obj = new JSONObject(json);
+		Integer memberNo = obj.isNull("memberNo") ? null : obj.getInt("memberNo");
+		String lastLoginTime = obj.isNull("lastLoginTime") ? null : obj.getString("lastLoginTime");
 
-		Member member = (Member) session.getAttribute("loggedInUser");
-
-		if (member != null) {
-			member.setLastLoginTime(new Date());
-
-			memberService.logout(member);
+		if (memberNo == null || lastLoginTime == null) {
+			responseJson.put("success", false);
+			responseJson.put("message", "用戶未登錄");
+			return responseJson.toString();
 		}
 
-		session.invalidate();
+		Member updatedMember = memberService.logout(json);
 
-		return "users/logoutPage";
+		if (updatedMember != null) {
+			session.removeAttribute("loginMember");
+			session.removeAttribute("lastLoginTime");
+
+			responseJson.put("success", true);
+			responseJson.put("message", "登出成功");
+		}
+		return responseJson.toString();
 	}
+
 }
