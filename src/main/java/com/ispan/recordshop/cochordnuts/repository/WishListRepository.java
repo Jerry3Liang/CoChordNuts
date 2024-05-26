fe926700272007533e9dfdbb1e4f5ca71a485dc1
Abby <abbywupp@gmail.com>
2024-05-26 22:49:41 +0800

    新增 wishlist

---
+++ src/main/java/com/ispan/recordshop/cochordnuts/repository/WishListRepository.java
@@ -0,0 +1,9 @@
+package com.ispan.recordshop.cochordnuts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ispan.recordshop.cochordnuts.model.WishList;
+
+public interface WishListRepository extends JpaRepository<WishList, Integer> {
+
+}
